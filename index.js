764ca65462e5571b5e94e082e2835443341a1b01
Yoshihide Taniguchi (ravelll) <ravelll.taniguchi@gmail.com>
2020-12-11 16:48:43 +0900

    Add configuration files

---
+++ index.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "@typescript-eslint"
+  ],
+  "rules": {
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "import/extensions": "off",
+    "import/no-unresolved": "off",
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+  ]
+}
